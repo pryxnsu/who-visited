908e67012f03cfdea80d6de1ee0024cbae92a2bc
Priyanshu Kumar <priyanshuknp444@gmail.com>
2026-03-04 01:08:36 +0530

    feat: configure standalone output and production compiler settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === 'production'
+        ? {
+            exclude: ['error', 'warn'],
+          }
+        : false,
+  },
+  devIndicators: process.env.NODE_ENV === 'production' ? false : { position: 'bottom-left' },
+  reactCompiler: true,
+  output: 'standalone',
 };
 
 export default nextConfig;
